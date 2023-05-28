3609fc51825a8069b3c75c709eb7543eebb406ed
Mykhailo Aloshyn <reklama.alatyr@gmail.com>
2023-05-28 17:34:00 +0300

    feat:added email validation on input

--- currency-rate/app/Http/Controllers/EmailSubscriptionController.php
+++ currency-rate/app/Http/Controllers/EmailSubscriptionController.php
@@ -6,15 +6,24 @@ use App\Services\EmailSubscriptionService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Validator;
 
 class EmailSubscriptionController extends Controller
 {
     public function subscribeEmail(Request $request, EmailSubscriptionService $subscriptionService): JsonResponse
     {
-        $email = $request->get('email');
-        if (empty($email)) return response()->json(['error' => 'No email address was provided'], Response::HTTP_BAD_REQUEST);
+        $validator = Validator::make($request->all(), $this->emailRule());
+        if ($validator->fails()) return response()->json(['error' => $validator->errors()], Response::HTTP_BAD_REQUEST);
+        $email = $validator->validated()['email'];
         if ($subscriptionService->isExist($email)) return response()->json(['error' => 'Email is already subscribed'], Response::HTTP_CONFLICT);
         $subscriptionService->subscribe($email);
         return response()->json(['success' => true, 'message' => 'Email subscribed successfully'], Response::HTTP_OK);
+    }
+
+    private function emailRule(): array
+    {
+        return [
+            'email' => 'required|regex:/^[\w]+(\.[\w-]+)*@\w+(\.[\w]+)*$/',
+        ];
     }
 }
